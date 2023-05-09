9b1727ad84c4bf21624789c4fd4ad089c219d203
colins-uky <colin.schuh@uky.edu>
2023-05-09 10:40:55 -0400

    Work on sorting page.

--- src/pages/projects/conways.tsx
+++ src/pages/projects/conways.tsx
@@ -32,7 +32,7 @@ export default function Conways() {
 
             <h1 className="text-yellow text-8xl mt-10 conways text-center [text-shadow:_2px_2px_3px_rgb(255_255_255_/_80%)]"> Conway's Game of Life</h1>
 
-            <div className="flex flex-col bg-rblack h-3/4 max-h-lg w-3/4 max-w-screen-lg mt-10 rounded-3xl p-5"
+            <div className="flex flex-col bg-rblack h-3/4 max-h-lg w-3/4 max-w-screen-lg mt-10 rounded-3xl p-5 shadow-lg shadow-rblack"
                 onContextMenu={(e) => e.preventDefault()}
             >
                 <Grid 
@@ -47,10 +47,16 @@ export default function Conways() {
 
 
         <div className="flex h-[75vh] bg-jet justify-center items-center">
-            <div className="flex flex-col w-3/4 max-w-screen-lg h-3/4 bg-rblack rounded-3xl p-10">
+            <div className="flex flex-col w-3/4 max-w-screen-lg h-3/4 bg-rblack rounded-3xl p-10 shadow-lg shadow-rblack">
                 <h1 className="w-full text-center text-3xl text-bright font-bold">Conway's Game of Life</h1>
 
-                <p className="text-bright text-lg font-serif indent-10 mt-10">The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970. It is a zero-player game, meaning that its evolution is determined by its initial state, requiring no further input. One interacts with the Game of Life by creating an initial configuration and observing how it evolves. It is Turing complete and can simulate a universal constructor or any other Turing machine.</p>
+                <p className="text-bright text-lg font-serif indent-10 mt-10">
+                    The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970.
+                    It is a zero-player game, meaning that its evolution is determined by its initial state, 
+                    requiring no further input. One interacts with the Game of Life by creating an initial configuration and observing how it evolves.
+                    It is Turing complete and can simulate a universal constructor or any other Turing machine.
+                    
+                </p>
 
                 <a className="mt-5 text-yellow text-lg underline hover:text-cambridge" href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life" target="_blank">Conway's Game of Life Wikipedia</a>
             </div>
--- src/pages/projects/sorting.tsx
+++ src/pages/projects/sorting.tsx
@@ -16,7 +16,9 @@ export default function Sorting() {
             <title>Conway's Game of Life</title>
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="icon" href='favicon.svg' />
-            
+            <style>
+                @import url('https://fonts.googleapis.com/css2?family=Bruno+Ace+SC&display=swap');
+            </style>
         </Head>
 
         <div className="flex flex-col items-center w-screen h-screen bg-jet">
@@ -24,11 +26,11 @@ export default function Sorting() {
 
             <Topbar />
 
-            <h1 className="text-munsell font-Lato font-bold text-8xl mt-10 conways text-center [text-shadow:_2px_2px_5px_rgb(98_146_158_/_100%)]">
+            <h1 className="text-munsell sorting  font-bold text-8xl mt-10 text-center [text-shadow:_2px_2px_5px_rgb(98_146_158_/_100%)]">
                 Sorting
             </h1>
 
-            <div className="flex w-3/4 h-full bg-rblack rounded-3xl mt-[5vh]">
+            <div className="flex w-3/4 max-w-screen-lg h-3/4 max-h-lg bg-rblack rounded-3xl mt-[5vh] shadow-lg shadow-rblack">
 
             </div>
 
