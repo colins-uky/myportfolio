aaf62411bc8e5a92f8d030a75fbe0ad348001fa0
colins-uky <colin.schuh@uky.edu>
2023-07-14 14:35:23 -0400

    Started on weather api.

---
+++ src/pages/api/weather.ts
@@ -0,0 +1,21 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import axios from 'axios';
+
+
+
+const apiKey = process.env.OPEN_WEATHER_MAP_API_KEY;
+
+
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+
+
+
+    
+
+
+
+}
